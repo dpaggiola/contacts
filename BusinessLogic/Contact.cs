13fb39e90560cee89a89e8c1c899c2d5bc1f6c39
Daniel Paggiola <dpaggiola@gmail.com>
2022-09-02 01:41:23 -0300

    [GREEN] age limit implementation

--- BusinessLogic/Contact.cs
+++ BusinessLogic/Contact.cs
@@ -4,11 +4,22 @@ namespace BusinessLogic
 {
     public class Contact
     {
+        private DateTime birthday;
+
         public Contact()
         {
         }
 
         public string Name { get; set; }
-        public DateTime Birthday { get; set; }
+        public DateTime Birthday { 
+            get => birthday; 
+            set { 
+                if (value <= new DateTime(1910, 1, 1))
+                {
+                    throw new ArgumentException($"Birthday must be after {new DateTime(1910, 1, 1)}");
+                }
+                birthday = value;
+            } 
+        }
     }
 }
